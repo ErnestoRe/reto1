329e8160ed3b5de6352773fea5f7779e71dc5696
ErnestoRe <ernestorezustaaznarez@gmail.com>
2024-10-29 10:32:02 +0100

    Commit main mod

--- Reto1/src/retos/Reto1.java
+++ Reto1/src/retos/Reto1.java
@@ -6,9 +6,9 @@ public class Reto1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n=0;
-		int contador=0;
-		int n1=0;
+		int n = 0;
+		int n1 = 0;
+		int n2 = 0;
 		System.out.println("Elige la operacion");
 		System.out.println("1.Sumar");
 		System.out.println("2.Restar");
@@ -19,24 +19,50 @@ public class Reto1 {
 		System.out.println("7.Positivo");
 		System.out.println("8.Negativo");
 		System.out.println("0.Salir");
-		
+
 		do {
 			try {
 				System.out.println("Introduce el nº de la operacion que desea");
-				String s=sc.nextLine();
-				n=Integer.parseInt(s);
-				if(n==1) {
-					System.out.println("Introduce un nº");
-					for(int i=0; i<=2; i++) {
-						String s=sc.nextLine();
-						n1=Integer.parseInt(s);
+				String s = sc.nextLine();
+				n = Integer.parseInt(s);
+				do {
+					try {
+						System.out.println("Introduce un nº");
+						String s1 = sc.nextLine();
+						n1 = Integer.parseInt(s1);
+						if (n >= 1 && n <= 4) {
+							System.out.println("Introduce otro nº");
+							String s2 = sc.nextLine();
+							n2 = Integer.parseInt(s2);
+						}
+						break;
+					} catch (Exception ex) {
+						System.out.println("Introduce un numero valido");
 					}
+				} while (true);
+				if (n == 1) {
+					System.out.println(Funciones2.suma(n1, n2));
+				} else if (n == 2) {
+					System.out.println(Funciones2.resta(n1, n2));
+				} else if (n == 3) {
+					System.out.println(Funciones1.multiplicacion(n1, n2));
+				} else if (n == 4) {
+					System.out.println(Funciones1.division(n1, n2));
+				} else if (n == 5) {
+					System.out.println(Funciones2.esPar(n1));
+				} else if (n == 6) {
+					System.out.println(Funciones2.esImpar(n1));
+				} else if (n == 7) {
+					System.out.println(Funciones1.esPositivo(n1));
+				} else if (n == 8) {
+					System.out.println(Funciones1.esNegativo(n1));
 				}
-				
-			}catch(Exception ex) {
+
+			} catch (Exception ex) {
 				System.out.println("Introduce un formato valido");
 			}
-		}while(n!=0);
+		} while (n != 0);
+		sc.close();
 
 	}
 
