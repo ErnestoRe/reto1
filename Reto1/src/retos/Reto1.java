0e48ccfca4dcf327c4509b15ffed03a00d39bdc8
ErnestoRe <ernestorezustaaznarez@gmail.com>
2024-10-29 09:46:04 +0100

    Primer commit main

--- Reto1/src/retos/Reto1.java
+++ Reto1/src/retos/Reto1.java
@@ -1,9 +1,36 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n1=0;
+		int contador=0;
+		
+		System.out.println("Elige la operacion");
+		System.out.println("1.Sumar");
+		System.out.println("2.Restar");
+		System.out.println("3.Multiplicar");
+		System.out.println("4.Dividir");
+		System.out.println("5.Par");
+		System.out.println("6.Impar");
+		System.out.println("7.Positivo");
+		System.out.println("8.Negativo");
+		System.out.println("0.Salir");
+		
+		do {
+			try {
+				System.out.println("Introduce el nº de la operacion que desea");
+				String s=sc.nextLine();
+				n1=Integer.parseInt(s);
+				
+				
+			}catch(Exception ex) {
+				System.out.println("Introduce un formato valido");
+			}
+		}while(true);
 
 	}
 
